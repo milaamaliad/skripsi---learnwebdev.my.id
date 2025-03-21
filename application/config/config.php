4ed27fb4060d6f654077fc4023e23b8bdb62bb36
Milaamaliad <78720205+milaamaliad@users.noreply.github.com>
2025-03-21 16:00:07 +0700

    Update base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://learnwebdev.my.id/';
+$config['base_url'] = 'https://milaamaliad.github.io/skripsi---learnwebdev.my.id/';
 
 /*
 |--------------------------------------------------------------------------
